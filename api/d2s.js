da0e0543fde512a2788918a1497e90520b9244d2
unknown <dschu012@gmail.com>
2020-09-03 07:57:17 -0400

    wip

--- api/d2s.js
+++ api/d2s.js
@@ -2,24 +2,26 @@ const fetch = require('node-fetch');
 const d2s = require('@dschu012/d2s');
 const constants = require('../lib/constants.bundle.min.json');
 
-exports.fetch_chars = async function (names) {
-  let data = [];
-  names = names.split(/[,\s]/).map(d => d.toLowerCase().trim());
-  for (const n of names) {
-    let response = await fetch(`https://armory.slashdiablo.net/retrieving/v1/character?name=${n}`);
+async function fetch_char(n) {
+  let response = await fetch(`https://armory.slashdiablo.net/retrieving/v1/character?name=${n}`);
     if (response.ok) {
       let json = await response.json();
       try {
         let binary = await d2s.write(json.character.d2s, constants);
         json = await d2s.read(binary, constants);
-        data.push({ name: n, data: json });
+        return { name: n, data: json };
       } catch (e) {
         console.log(e);
-        data.push({ name: n, data: null });
+        return { name: n, data: null };
       }
     } else {
-      data.push({ name: n, data: null });
+      return { name: n, data: null };
     }
-  }
+}
+
+exports.fetch_chars = async function (names) {
+  let data = [];
+  names = names.split(/[,\s]/).map(d => d.toLowerCase().trim());
+  data = Promise.all(names.map(e => fetch_char(e)));
   return data;
 }
